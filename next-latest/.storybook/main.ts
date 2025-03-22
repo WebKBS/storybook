49c9d411c79a3dd8b8a9903c9e1e895514fb3bcd
kangbeomsu <kbs21c1987@gmail.com>
2025-03-22 19:58:53 +0900

    init

---
+++ next-latest/.storybook/main.ts
@@ -0,0 +1,14 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+  ],
+  framework: '@storybook/nextjs',
+  staticDirs: ['../public'],
+};
+export default config;
